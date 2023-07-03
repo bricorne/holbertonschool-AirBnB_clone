9f0851e80f38a4d4f0e2cecfd03cd2f42a0dbb63
Nico-dsa <6463@holbertonstudenst.com>
2023-07-03 15:45:56 +0200

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ from datetime import datetime
 
 class BaseModel:
     """Represents the BaseModel of the AirBnB project."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel"""
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tformat)
+                else:
+                    self.__dict__[k] = v
 
     def save(self):
         """Update updated_at with the current datetime."""
