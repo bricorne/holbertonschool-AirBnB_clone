20b2fd1faa79197b3e2204414cb6ca0e568c62c4
Nico-dsa <6463@holbertonstudenst.com>
2023-07-03 15:14:50 +0200

    Add commentary on function

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """create class BaseModel"""
 
-
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
+    """Represents the BaseModel of the AirBnB project."""
     def __init__(self):
+        """Initialize a new BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def save(self):
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Return the dictionary of the BaseModel instance."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
@@ -22,4 +26,5 @@ class BaseModel:
         return obj_dict
 
     def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
