48a1a6b610df96c33c6ca64c68c0c145f9e24972
Nico-dsa <6463@holbertonstudenst.com>
2023-07-04 13:28:24 +0200

    Store first object

---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""create class FileStorage"""
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+	"""class that serializes instances to a JSON file and \
+	deserializes JSON file to instances"""
+	__file_path = "file.json"
+	__objects = {}
+
+	def all(self):
+		"""Return the dictionary __objects"""
+		return FileStorage.__objects
+
+	def new(self, obj):
+		"""Sets in __objects the obj with key <obj class name>.id"""
+		objname = obj.__class__.__name__
+		FileStorage.__objects["{}.{}".format(objname, obj.id)] = obj
+
+	def save(self):
+		"""serializes __objects to the JSON file (path: __file_path)"""
+		odict = FileStorage.__objects
+		objdict = {obj: odcit[obj].to_dict() for obj in odict.keys()}
+		with open(FileStorage.__file_path, "w") as f:
+			json.dump(objdict, f)
+
+	def reload(self):
+		"""deserializes the JSON file to __objects"""
+		try:
+			with open(FileStorage.__file_path) as f:
+				objdict = json.load(f)
+				for o in objdict.values():
+					cls_name = o["__class__"]
+					del o["__class__"]
+					self.new(eval(cls_name)(**o))
+		except FileNotFoundError:
+			return
