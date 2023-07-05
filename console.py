72f83803bc3e4c9608573c2585fb3e933df8917c
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-07-05 16:24:36 +0200

    console create show destroy all

--- console.py
+++ console.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """create a console"""
 import cmd
-
+import json
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     """create a prompt"""
     prompt = '(hbnb) '
+    list_class = ['BaseModel']
 
     def do_EOF(self, arg):
         """Exit the program using EOF"""
@@ -18,6 +21,86 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do anything"""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+            return
+        class_name = arg.strip()
+        try:
+            model_class = eval(class_name)
+        except NameError:
+            print("** class doesn't exist **")
+            return
+        new_instance = model_class()
+        new_instance.save()
+        print(new_instance.id)
+    
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        class_name = args[0]
+
+        if class_name not in HBNBCommand.list_class:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        inst_id = args[1]
+
+        key = "{}.{}".format(class_name, inst_id)
+        instances = FileStorage().all()
+
+        if key in instances:
+            instance = instances[key]
+            print(instance)
+        else:
+            print('** no instance found **')
+        
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name"""
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        class_name = args[0]
+
+        if class_name not in HBNBCommand.list_class:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        inst_id = args[1]  
+
+        try:
+            obj_to_del = FileStorage().all()
+            string = "{}.{}".format(class_name, inst_id)
+            del obj_to_del[string]
+            FileStorage().save()
+        except:
+            print('** no instance found **')
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        arg = arg.split()
+        objects = FileStorage().all()
+
+        if len(arg) == 0:
+            print([str(obj) for obj in objects.values()])
+        elif arg[0] not in HBNBCommand.list_class:
+            print("** class doesn't exist **")
+        else:
+            print([str(obj) for obj in objects.values()
+                   if type(obj).__name__ == arg[0]])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
